5c1e6d9c03d2176d977cffd0801d354e2ff66a2c
Daniel Ramirez <daniel_r129@hotmail.com>
2021-02-01 19:01:57 -0800

    Created Electronic class

---
+++ TeamProjectTracker/Electronic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProjectTracker
+{
+    public class Electronic
+    {
+        /// <summary>
+        /// The unique identification number for a specific product
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// The entire name of the product
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The name of the manufacturer where the product was made
+        /// </summary>
+        public string Manufacturer { get; set; }
+
+        /// <summary>
+        /// The price of the product in US currency
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// The category to which the product belongs to
+        /// </summary>
+        public string Category { get; set; }
+    }
+}
