a0de83e69b227a1ea6c050e4948dde5cb443bd2c
Daniel Ramirez <daniel_r129@hotmail.com>
2021-02-01 19:21:40 -0800

    Added initial migration and created the TrackingDb

--- TeamProjectTracker/Electronic.cs
+++ TeamProjectTracker/Electronic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace TeamProjectTracker
         /// <summary>
         /// The unique identification number for a specific product
         /// </summary>
+        [Key]
         public int ProductId { get; set; }
 
         /// <summary>
